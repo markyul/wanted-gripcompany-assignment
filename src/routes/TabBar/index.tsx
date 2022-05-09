a09970ef391da1f53874f678987db4ee8b594c0a
markyul <pig68111000@gmail.com>
2022-05-10 01:31:07 +0900

    Tab bar UI 구현

--- src/routes/TabBar/index.tsx
+++ src/routes/TabBar/index.tsx
@@ -1,7 +1,12 @@
 import styles from './TabBar.module.scss'
 
 const TabBar = () => {
-  return <nav className={styles.tabContainer}>asd</nav>
+  return (
+    <nav className={styles.tabContainer}>
+      <button type='button'>Search</button>
+      <button type='button'>Book Mark</button>
+    </nav>
+  )
 }
 
 export default TabBar
